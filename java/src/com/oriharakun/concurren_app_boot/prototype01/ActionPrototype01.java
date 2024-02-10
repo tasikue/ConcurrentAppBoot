77488a1e26fe1cb7c791e869d0223763a681a290
Masayuki Orihara <tasikue@gmail.com>
2024-02-10 22:45:00 +0900

    add aprication lunch protetype

--- java/src/com/oriharakun/concurren_app_boot/prototype01/ActionPrototype01.java
+++ java/src/com/oriharakun/concurren_app_boot/prototype01/ActionPrototype01.java
@@ -2,11 +2,16 @@ package com.oriharakun.concurren_app_boot.prototype01;
 
 import javax.swing.*;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 public class ActionPrototype01 implements ActionListener{
 
+    // 変数
     JButton button;
+    String[] argAtomPath = {"C:\\Users\\swing\\AppData\\Local\\atom\\atom.exe", "01"};
+    String[] argFfftpPath = {"C:\\Users\\swing\\Documents\\ソフトウェア\\ffftp-v5.8-x64\\ffftp.exe", "01"};
+
 
     public ActionPrototype01(JButton button){
         this.button = button;
@@ -16,7 +21,21 @@ public class ActionPrototype01 implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent event){
         if(event.getSource() == button) {
-            System.out.println("!");
+            openApp(argAtomPath);
+            openApp(argFfftpPath);
+        }
+    }
+
+    /**
+     * パスで指定したアプリケーションを起動
+     */
+    public void openApp(String[] path){
+        String[] argAppPath = path;
+
+        try {
+            Runtime.getRuntime().exec(argAppPath);
+        } catch(IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- java/src/com/oriharakun/concurren_app_boot/prototype01/AppPrototype01.java
+++ java/src/com/oriharakun/concurren_app_boot/prototype01/AppPrototype01.java
@@ -6,35 +6,35 @@ import java.awt.Container;
 
 public class AppPrototype01 {
 
+
     public static void main(String[] args) {
 
-    // final
-    final String FRAME_TITLE = "同時アプリケーション起動ツール（仮）";
-    final int WINDOW_WIDTH = 320;
-    final int WINDOW_HEIGHT = 180;
+        // final
+        final String FRAME_TITLE = "同時アプリケーション起動ツール（仮）";
+        final int WINDOW_WIDTH = 240;
+        final int WINDOW_HEIGHT = 360;
 
-    // 変数
+        // 変数
+        // インスタンス
+        JFrame mainFrame = new JFrame(FRAME_TITLE);
+        JLabel label = new JLabel("SwingLabel");
+        JButton button = new JButton("SwingButton");
+        ActionPrototype01 ap01 = new ActionPrototype01(button);
 
-    // インスタンス
-    JFrame mainFrame = new JFrame(FRAME_TITLE);
-    JLabel label = new JLabel("SwingLabel");
-    JButton button = new JButton("SwingButton");
-    ActionPrototype01 ap01 = new ActionPrototype01(button);
+        // フレームの設定 
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        mainFrame.setLocationRelativeTo(null);
 
-    // フレームの設定 
-    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    mainFrame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-    mainFrame.setLocationRelativeTo(null);
-
-    // ラベルの設定
-    Container contentPane = mainFrame.getContentPane();
-    contentPane.add(label, BorderLayout.CENTER);
+        // ラベルの設定
+        Container contentPane = mainFrame.getContentPane();
+        contentPane.add(label, BorderLayout.CENTER);
     
-    // ボタンの設定
-    contentPane.add(button, BorderLayout.SOUTH);
-    button.addActionListener(ap01);
+        // ボタンの設定
+        contentPane.add(button, BorderLayout.SOUTH);
+        button.addActionListener(ap01);
 
-    // ウインドウを表示
-    mainFrame.setVisible(true);
+        // ウインドウを表示
+        mainFrame.setVisible(true);
     }
 }
